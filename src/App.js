1c8e51b0f0842779280bc902fc4afcdc300b1de0
PixelCook <zacharyigould@gmail.com>
2021-05-20 20:16:09 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import "../src/App.css"
+import HomeScreen from "./components/HomeScreen";
+import ProductScreen from "./components/ProductScreen";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import React from "react";
+
+function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  };
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  };
+  return (
+    <>
+      <BrowserRouter>
+        <div className="grid-container">
+          <header className="header">
+            <div className="brand">
+              <button onClick={openMenu} className="hamburger">
+                &#9776;
+              </button>
+              <Link to="/">Amazona</Link>
+            </div>
+            <div className="header-links">
+              <a href="signin.html">Signin</a>
+              <a href="cart.html">Cart</a>
+            </div>
+          </header>
+          <aside className="sidebar">
+            <h3>Shopping Categories</h3>
+            <button className="sidebar-close" onClick={closeMenu}>
+              x
+            </button>
+            <ul>
+              <li>
+                <a href="index.html">Pants</a>
+              </li>
+              <li>
+                <a href="index.html">Shirts</a>
+              </li>
+            </ul>
+          </aside>
+          <main className="main">
+            <div className="content">
+              <Route path="/product/:id" component={ProductScreen} />
+              <Route path="/" exact={true} component={HomeScreen} />
+            </div>
+          </main>
+          <footer className="footer">All rights reserved</footer>
+        </div>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ProductScreen.jsx
@@ -0,0 +1,53 @@
+import react from "react";
+import data from "../data";
+import { Link } from "react-router-dom";
+
+function ProductScreen(props) {
+  const product = data.products.find((x) => x._id === props.match.params.id);
+  return (
+    <div>
+      <div>
+        <Link to="/" className="back-to-result">Back to Homepage</Link>
+      </div>
+      <div className="details">
+        <div className="details-image">
+          <img src={product.image} alt={product.name}></img>
+        </div>
+        <div className="details-info">
+          <ul>
+            <li>
+              <h4>{product.name}</h4>
+            </li>
+
+            <li>
+              {product.rating} Stars ({product.reviewNum} reviews)
+            </li>
+            <li>
+              Price:<b>${product.price}</b>
+            </li>
+            <li>
+              <p>Product Description: {product.description}</p>
+            </li>
+          </ul>
+        </div>
+        <div className="details-action">
+          <ul>
+            <li>Price ${product.price}</li>
+            <li>
+              QTY{" "}
+              <select>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </select>
+            </li>
+            <button>Add to Cart</button>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductScreen;
